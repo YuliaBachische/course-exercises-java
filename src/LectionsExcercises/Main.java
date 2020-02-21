208c80a931271390a65488e502d688b6ae3f2f18
Asus <juliabach1@mail.ru>
2020-02-21 14:59:16 +0300

    Initial commit

---
+++ src/LectionsExcercises/Main.java
@@ -0,0 +1,28 @@
+package LectionsExcercises;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите два слова через пробел:");
+        String line1 = sc.next();
+        String line2 = sc.next();
+        line1.equals(line2);
+        line1.equalsIgnoreCase(line2);
+        line1.length();
+        line2.length();
+        if (line1.equals(line2)) {
+            System.out.println("Отлично. Слова одинаковы.");
+        } else if (line1.equalsIgnoreCase(line2)) {
+            System.out.println("Хорошо. Почти одинаковы");
+        } else if (line1.length() == line2.length()) {
+            System.out.println("Ну хотя бы одной длины");
+        } else {
+            System.out.println("Они совсем разные");
+            // write your code here
+        }
+
+    }
+}
