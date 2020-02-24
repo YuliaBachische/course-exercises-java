13b8489f19c60e21be3558a640a6f45e2bba6607
Asus <juliabach1@mail.ru>
2020-02-24 21:41:45 +0300

    Add code classes

---
+++ src/LectionsExcercises2/Excercise7.java
@@ -0,0 +1,17 @@
+package LectionsExcercises2;
+
+import java.util.Scanner;
+
+public class Excercise7 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int num = sc.nextInt();
+        int mul = 1;
+        for(int i = 1; i <= num; i++){
+            mul *= i;
+        }
+        System.out.println("Факториал числа: ");
+        System.out.println(mul);
+    }
+}
