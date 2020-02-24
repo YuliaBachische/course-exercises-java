fafa4a492a4705e300ef37e5ddcdacff8d99fd15
Asus <juliabach1@mail.ru>
2020-02-24 12:36:22 +0300

    Add code classes

---
+++ src/LectionsExcercises2/Excercise1.java
@@ -0,0 +1,24 @@
+package LectionsExcercises2;
+
+public class Excercise1 {
+    public static void main(String[]args){
+        int[] array = new int[5];
+        array[0] = 8;
+        array[1] = 10;
+        array[2] = 6;
+        array[3] = 0;
+        array[4] = 12;
+        System.out.println("Обычный порядок: ");
+        for(int i = 0; i< array.length; i++){
+            System.out.println(array[i]);
+        }
+        int[] array2 = new int[5];
+        for(int i = 0; i < array2.length; i++){
+                array2[4-i] = array[i];
+            }
+        System.out.println("Обратный порядок: ");
+        for(int i = 0; i < array2.length; i++){
+            System.out.println(array2[i]);
+        }
+    }
+}
