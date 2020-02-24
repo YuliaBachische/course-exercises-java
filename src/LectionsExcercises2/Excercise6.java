d9c2ea7df404d6d7b528f6521ec106446e39bd7d
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-02-24 21:55:38 +0300

    Update Excercise6.java

--- src/LectionsExcercises2/Excercise6.java
+++ src/LectionsExcercises2/Excercise6.java
@@ -9,17 +9,12 @@ public class Excercise6 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число: ");
         int num = sc.nextInt();
-        for (int i = (int) sqrt(num); i > 0; i--) {
             if (IsSimple(num) == true) {
                 System.out.println("Число является простым");
-                break;
             } else {
                 System.out.println("Число не является простым");
-                break;
             }
         }
-    }
-
     private static boolean IsSimple(int num) {
         for (int i = 2; i < num; i++)
         {
