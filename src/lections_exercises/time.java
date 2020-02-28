dd1dfd492e65cb6dea6f2a53851b2436f380952a
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-02-28 10:41:21 +0300

    Update time.java

--- src/lections_exercises/time.java
+++ src/lections_exercises/time.java
@@ -1,38 +1,38 @@
 package lections_exercises;
 
 public class time {
-        public static int sec;
-        public static int min;
-        public static int hour;
-        public static void main(String[]args) {
-            time sec = new time(-45463);
-            sec.showTime();
+    public  int sec;
+    public  int min;
+    public  int hour;
+    public static void main(String[]args) {
+        time sec = new time(5557);
+        sec.showTime();
+    }
+    public  void showTime() {
+        this.min = sec/60;
+        this.sec %= 60;
+        this.hour = min/60;
+        if(sec<=0){
+            System.out.println(0);
         }
-        public static void showTime() {
-            time.min = sec/60;
-            time.sec %= 60;
-            time.hour = min/60;
-            if(sec<=0){
-                System.out.println(0);
-            }
-            if(min>=60) {
-                min = min - hour * 60;
-                System.out.println("Часы: " + hour + " " + " Минуты: " + min + " " + " " + "Секунды: " + sec);
-            }
-        }
-        public time (int sec){
-            this.sec = sec;
-        }
-        public static void showSec(){
-            time.min = min*60;
-            time.hour = hour*3600;
-            time.sec = sec;
-            System.out.println(min + hour + sec);
-        }
-        public time(int sec, int min, int hour){
-            this.min = min;
-            this.hour = hour;
-            this.sec = sec;
+        if(min>=60) {
+            min = min - hour * 60;
+            System.out.println("Часы: " + hour + " " + " Минуты: " + min + " " + " " + "Секунды: " + sec);
         }
     }
+    public time (int sec){
+        this.sec = sec;
+    }
+    public void showSec(){
+        this.min = min*60;
+        this.hour = hour*3600;
+        System.out.println(min + hour + sec);
+    }
+    public time(int sec, int min, int hour){
+        this.min = min;
+        this.hour = hour;
+        this.sec = sec;
+    }
+}
+
 
