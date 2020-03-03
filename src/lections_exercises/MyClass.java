a4e59f3ae830a012cda98bad98aedc88d5500fe6
Asus <juliabach1@mail.ru>
2020-03-03 20:56:42 +0300

    Add code classes

---
+++ src/lections_exercises/MyClass.java
@@ -0,0 +1,36 @@
+package lections_exercises;
+import java.math.BigDecimal;
+
+public class MyClass {
+    int a;
+    int b;
+    BigDecimal s;
+    BigDecimal t;
+
+    public MyClass(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public double MyCl(){
+        double mul = a * b;
+        return mul;
+    }
+    public MyClass(BigDecimal s, BigDecimal t){
+        this.s = s;
+        this.t = t;
+    }
+    public BigDecimal MyCls(){
+        BigDecimal mul = s.multiply(t);
+        return mul;
+    }
+
+    public static void main(String[] args) {
+        MyClass d = new MyClass(BigDecimal.valueOf(823.49), BigDecimal.valueOf(9.238));
+        System.out.println(d.MyCls());
+
+
+
+
+
+    }
+}
