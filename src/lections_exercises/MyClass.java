8f7e7c3e0e40dc057220988d76f28421c683b9aa
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-05 14:59:02 +0300

    Update MyClass.java

--- src/lections_exercises/MyClass.java
+++ src/lections_exercises/MyClass.java
@@ -1,20 +1,32 @@
+/*
+Определить класс, который при инициализации принимает на
+вход 2 аргумента целого или вещественного типа. Задать
+перегруженные методы
+возвращающие результат арифметических
+операций над заданными полями и полями типа
+BigDecimal
+ */
 package lections_exercises;
 import java.math.BigDecimal;
 
 public class MyClass {
     int a;
     int b;
-    BigDecimal s;
-    BigDecimal t;
-    public double MyClass(int a, int b){
-        this.a = a;
-        this.b = b;
+    double a1;
+    double b1;
+    public double MyClass(double a1, double b1){
+        this.a1 = a1;
+        this.b1 = b1;
         double mul = a * b;
         return mul;
     }
+    public int MyClass(int a, int b){
+        this.a = a;
+        this.b = b;
+        int mul = a * b;
+        return mul;
+    }
     public BigDecimal MyClass(BigDecimal s, BigDecimal t){
-        this.s = s;
-        this.t = t;
         BigDecimal mul = s.multiply(t);
         return mul;
     }
@@ -23,4 +35,3 @@ public class MyClass {
         System.out.println(d.MyClass(3,5));
     }
 }
-
