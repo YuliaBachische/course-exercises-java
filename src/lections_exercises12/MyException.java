6f264fd05131d2bb2a6ae24fabbddaa7c10d69be
Asus <juliabach1@mail.ru>
2020-03-10 18:46:11 +0300

    Add code classes

---
+++ src/lections_exercises12/MyException.java
@@ -0,0 +1,23 @@
+package lections_exercises12;
+
+import org.apache.log4j.Logger;
+
+public class MyException extends Exception {
+    private static Logger LOGGER = Logger.getLogger(MyException.class);
+
+
+    public static int getPositiveResult(int num) throws MyException{
+        if(num<0) throw new MyException();
+        else return num*num;
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyException.getPositiveResult(-8);
+        } catch (MyException ex){
+            LOGGER.error("The result is negative");
+            ex.printStackTrace();
+        }
+
+    }
+}
