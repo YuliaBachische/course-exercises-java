6f4c04f0bc6438c5ae0a3896bb73821121280850
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-13 17:58:19 +0300

    Update Exercise4.java

--- src/lections_exercises14/Exercise4.java
+++ src/lections_exercises14/Exercise4.java
@@ -14,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Exercise4 {
+    private static Logger LOGGER = Logger.getLogger(Exercise4.class);
     public static void main(String[] args) {
         String str = "+375293651474 +37529365147 +37529365 +3752793651474 +375293651474 +3752936514748 +9803288909";
         Pattern p = Pattern.compile("^\\+375(\\d{2})\\d{7}$");
@@ -23,7 +24,8 @@ public class Exercise4 {
             m = p.matcher(array[i]);
             if(m.find()){
                 array[i] = array[i].replaceAll(m.group(1), "\\(" + m.group(1) + "\\)");
-                System.out.println(array[i]);
+                LOGGER.setLevel(Level.INFO);
+                LOGGER.info(array[i]);
             }
         }
     }
