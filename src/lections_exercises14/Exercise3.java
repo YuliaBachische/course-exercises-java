a6f9c8243fdd50428f61f4feac848e681d54d866
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-13 20:20:14 +0300

    Update Exercise3.java

--- src/lections_exercises14/Exercise3.java
+++ src/lections_exercises14/Exercise3.java
@@ -11,12 +11,21 @@ package lections_exercises14;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exercise3 {
     private static Logger LOGGER = Logger.getLogger(Exercise3.class);
     public static void main(String[] args) {
-        String str = "sfs <p id = p1> fef <p id = p1> seller ";
-        str = str.replace("<p id = p1>", "<p>");
-        LOGGER.setLevel(Level.INFO);
-        LOGGER.info(str);
+        String str = "sfs <p id = p1> fef <p id = p1> seller <p> kks";
+        Pattern p=Pattern.compile("<[^<>]+>");
+        Matcher m=p.matcher(str);
+        while (m.find()){
+           LOGGER.setLevel(Level.INFO);
+           LOGGER.info(m.start() + " " + m.end());
+           str = str.replaceAll(p.pattern(), "<p>");
+           LOGGER.info(str);
+        }
     }
 }
+
