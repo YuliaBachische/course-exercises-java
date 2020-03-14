63807d6fe2f410493c3124342bb2d4641c691a66
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-14 11:31:28 +0300

    Update Exercise4.java

--- src/lections_exercises14/Exercise4.java
+++ src/lections_exercises14/Exercise4.java
@@ -10,6 +10,9 @@ Y-номер телефона
 */
 package lections_exercises14;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,12 +20,12 @@ public class Exercise4 {
     private static Logger LOGGER = Logger.getLogger(Exercise4.class);
     public static void main(String[] args) {
         String str = "+375293651474 +37529365147 +37529365 +3752793651474 +375293651474 +3752936514748 +9803288909";
-        Pattern p = Pattern.compile("^\\+375(\\d{2})\\d{7}$");
+        Pattern p = Pattern.compile("^\\+375(\\d{2})\\d{3}(\\d{2})\\d{2}$");
         Matcher m;
         String [] array = str.split(" ");
         for(int i = 0; i < array.length; i++){
             m = p.matcher(array[i]);
-            if(m.find()){
+            if(m.matches()){
                 array[i] = array[i].replaceAll(m.group(1), "\\(" + m.group(1) + "\\)");
                 array[i] = array[i].replaceAll(m.group(2), "\\-" + m.group(2) + "\\-");
                 LOGGER.setLevel(Level.INFO);
