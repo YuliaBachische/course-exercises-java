32a93251c138d0aee938b0eb811c86f90cd3a99c
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-17 13:21:55 +0300

    Update PairUtil.java

--- src/lections_exercises09/PairUtil.java
+++ src/lections_exercises09/PairUtil.java
@@ -1,3 +1,10 @@
+/*
+Создать статический обобщённый метод
+swap в final классе PairUtil.
+Метод должен принимать объект класса
+Pair и возвращать пару, в
+которой элементы поменяны местами
+*/
 package lections_exercises09;
 
 public final class PairUtil {
