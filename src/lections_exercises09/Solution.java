c43d9fa3c9993b7e089ef4fe32fa9302ec700ed6
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-17 13:23:14 +0300

    Update Solution.java

--- src/lections_exercises09/Solution.java
+++ src/lections_exercises09/Solution.java
@@ -1,3 +1,10 @@
+/*
+Дан класс Solution, параметризированный T. Ограничьте параметр
+T.T должен быть наследником класса
+ClassForGenerics и одновременно
+реализовывать интерфейс
+InterfaceForGenerics. Менять можно только класс Solution
+*/
 package lections_exercises09;
 
 public class Solution<T extends ClassForGenerics & InterfaceForGenerics>{
