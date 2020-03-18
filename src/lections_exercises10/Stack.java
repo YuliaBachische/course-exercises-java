267f23074e001e13a589b923d74853aa6232bf0a
Asus <juliabach1@mail.ru>
2020-03-18 20:59:39 +0300

    Add code classes

--- src/lections_exercises10/Stack.java
+++ src/lections_exercises10/Stack.java
@@ -1,24 +1,38 @@
+/*
+Ввести с консоли число, занести его цифры в стек. Стек – самостоятельно
+спроектированный класс, поддерживающий
+3 операции:
+1.)Задать размер стека
+2.)Внести цифру в стек.
+3.) Извлечь цифру из стека.
+Вывести число, у которого цифры идут в обратном порядке
+ */
 package lections_exercises10;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Stack {
-    ArrayList<Character> numbers = new ArrayList<>();
+    private static Logger LOGGER = Logger.getLogger(Stack.class);
+    static ArrayList<Character> numbers = new ArrayList<>();
     int size = numbers.size();
-    public void push(String num){
-        char [] chars = num.toCharArray();
-        for (int i = 0; i < chars.length ; i++) {
-            this.numbers.add(chars[i]);
+    public void push(String num) {
+        char[] chars = num.toCharArray();
+        for (char aChar : chars) {
+            numbers.add(aChar);
+            size++;
         }
-
     }
-    public int pop(int num){
+    public StringBuilder pop(String num){
+        char[] chars = num.toCharArray();
         StringBuilder str = new StringBuilder();
-        for (int i = numbers.size(); i >num; i++) {
-            str.append(numbers.get(numbers.size()));
+        for (int i = 1; i <=chars.length; i++) {
+            str.append(numbers.get(numbers.size() - i));
         }
-
+        return str;
     }
     public void setSize(int num){
         this.size = num;
@@ -26,6 +40,11 @@ public class Stack {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
         String enteredNumber = scanner.nextLine();
+        Stack stack = new Stack();
+        stack.push(enteredNumber);
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.info(stack.pop(enteredNumber));
     }
 }
