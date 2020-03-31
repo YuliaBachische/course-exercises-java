49bde545587329fbb4ad10419fa3dc430dfbab97
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-03-31 11:49:09 +0300

    Update Exercise4.java

--- src/lect_ex17/Exercise4.java
+++ src/lect_ex17/Exercise4.java
@@ -1,3 +1,10 @@
+/*
+Дан список целых чисел, вывести строку,
+представляющую собой конкатенацию строковых
+представлений этих чисел.
+Пример: список {5, 2, 4, 2, 1}
+Результирующая строка: "52421"
+*/
 package lect_ex17;
 
 import java.util.Arrays;
@@ -8,6 +15,7 @@ public class Exercise4 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(3,54,3,5,7);
         String res = list.stream()
+                .filter(Objects::nonNull)
                 .map(Object::toString)
                 .collect(Collectors.joining());
         System.out.println(res);
