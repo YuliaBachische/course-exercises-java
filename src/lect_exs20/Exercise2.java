9f87a8f739d80d38a147bd756bd52b01a2cd8c3b
YuliaBachische <61230122+YuliaBachische@users.noreply.github.com>
2020-04-07 20:14:15 +0300

    Update Exercise2.java

--- src/lect_exs20/Exercise2.java
+++ src/lect_exs20/Exercise2.java
@@ -12,8 +12,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Exercise2 {
     static int[] shared = new int[1000000];
-    
-    public static void main(String[] args) {
+    public static void main(String[] args){
         for (int i = 0; i < shared.length; i++) {
             shared[i] = (int) (Math.random()*300);
         }
@@ -29,23 +28,86 @@ public class Exercise2 {
                 }
             }
             finish.set(System.nanoTime());
-            System.out.println("single thread: " + (finish.get() - start.get()));
+            System.out.println("single thread1: " + (finish.get() - start.get()));
         });
         service.shutdown();
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        AtomicLong start2 = new AtomicLong();
-        AtomicLong finish2 = new AtomicLong();
-        executorService.submit(() -> {
+        AtomicLong start2 = new AtomicLong(System.nanoTime());
+        executorService.submit(new Thread(()->{
             int maxNumber = 0;
-            start2.set(System.nanoTime());
-            for (int value : shared) {
-                if (maxNumber < value) {
-                    maxNumber = value;
+            for (int j = 0; j < 100000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
                 }
             }
-            finish2.set(System.nanoTime());
-            System.out.println("threadPool: " + (finish2.get() - start2.get())); //ThreadPool быстрее
-        });
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 200000; j < 300000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 300000; j < 400000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 400000; j < 500000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 500000; j < 600000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 600000; j < 700000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 700000; j < 800000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 800000; j < 900000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        executorService.submit(new Thread(() -> {
+            int maxNumber = 0;
+            for (int j = 900000; j < 1000000; j++) {
+                if (maxNumber < shared[j]) {
+                    maxNumber = shared[j];
+                }
+            }
+        }));
+        AtomicLong finish2 = new AtomicLong(System.nanoTime());
         executorService.shutdown();
+        System.out.println("ThreadPool: " + (finish2.get()-start2.get()));
     }
 }
+
